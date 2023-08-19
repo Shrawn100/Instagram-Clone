bc484960238c289dcf5cf3d34aa05fc5b0c640ad
Shrawn <shrawn2002@gmail.com>
2023-08-19 23:17:03 +1000

    Set up JWT middleware

--- app.js
+++ app.js
@@ -51,3 +51,53 @@ app.use(function (err, req, res, next) {
 });
 
 module.exports = app;
+
+/* 
+---------------------- Things to do-------------------
+
+So what I need to do is, use the jwt library
+then I need to refactor the code where Im doing the try{jwt.verify}
+into a seperate middleware. I can set the authdata to req.authData=authData
+
+I think that will work because in the other jwt verifyToken middleware, we
+set the req.token = bearerToken; So that means that we can create req.x=y
+and then be able to access it further down.
+
+^^^ Yeah we definitely can, because in this code :
+
+router.get(
+  "/author",
+  verifyToken,  
+  asyncHandler(async (req, res, next) => {
+    try {
+      jwt.verify(req.token, process.env.SECRET, async (err, authData) => {
+        if (err) {
+          res.sendStatus(403);
+        } else {
+          let authorsBlogs = await Blog.find({
+            author: authData.user._id,
+          })
+            .sort({ date: -1 })
+            .populate("author")
+            .exec();
+
+          res.json(authorsBlogs);
+        }
+      });
+    } catch (error) {
+      res.sendStatus(403);
+    }
+  })
+);
+We are calling the verifyToken, and then straight after we are 
+accessing the token in jwt.verify
+
+that means if we refactor jwt.verify and set authData to req.authData
+Then we wont have to have that huge block of code and it will look 
+nice.
+Because all we are really trying to access from jwt.verify is the
+authData to begin with.
+
+
+
+*/
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+//Require the libraries
+require("dotenv").config();
+const { body, validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+/* ------- Set up JWT middleware ------- */
+
+function verifyAndDecodeToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers["authorization"];
+
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== "undefined") {
+    // Split header
+    const bearer = bearerHeader.split(" ");
+
+    // Get token from array
+    const bearerToken = bearer[1];
+
+    // Set the token
+    req.token = bearerToken;
+
+    // Verify the token
+    jwt.verify(req.token, process.env.SECRET, (err, authData) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        req.authData = authData;
+        next();
+      }
+    });
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+}
 
 module.exports = router;
