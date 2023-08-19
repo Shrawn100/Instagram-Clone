35332bb771b7e491fc288e83936add29fe909cae
Shrawn <shrawn2002@gmail.com>
2023-08-19 23:30:15 +1000

    Setup signup  route

--- routes/index.js
+++ routes/index.js
@@ -45,11 +45,45 @@ function verifyAndDecodeToken(req, res, next) {
   }
 }
 
-/* Setup login route */
+/* ---------- SIGNUP && LOGIN ROUTES --------- */
 
+router.post("/signup", [
+  body("username", "Username must be atleast 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("password", "Password must be atleast 6 characters")
+    .trim()
+    .isLength({ min: 6 })
+    .escape(),
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({ message: "Validation failed", errors: errors.array() });
+    }
+
+    const { username, password } = req.body;
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10); // Hash the password using bcrypt
+
+      const newUser = new User({
+        username,
+        password: hashedPassword, // Store the hashed password in the database
+      });
+
+      await newUser.save(); // Save the new user to the database
+
+      res.json({ message: "User registered successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "An error occurred" });
+    }
+  },
+]);
 router.post("/login", [
-  body("username").trim().isLength({ min: 1 }).escape(),
-  body("password").trim().isLength({ min: 1 }).escape(),
+  body("username", "Invalid username").trim().isLength({ min: 3 }).escape(),
+  body("password", "Invalid password").trim().isLength({ min: 6 }).escape(),
 
   asyncHandler(async (req, res) => {
     const errors = validationResult(req);
